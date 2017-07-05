6d911468a14307709b3f2a07665570664a0ce7f6
don <don@pn1251671.lanl.gov>
2017-07-05 16:52:33 -0600

    Added more information logging in try/except

--- lib/manage_keychain/manage_keychain.py
+++ lib/manage_keychain/manage_keychain.py
@@ -74,6 +74,8 @@ class ManageKeychain(object):
             functionName = str(inspect.stack()[2][3])
             lineNumber = str(inspect.stack()[2][2])
         except Exception, err:
+            logger.log(lp.WARNING, traceback.format_exc())
+            logger.log(lp.WARNING, str(err))
             raise err
         else:
             self.logger.log(lp.DEBUG, "called by: " + \
