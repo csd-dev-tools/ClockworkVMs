e1aa527a59163c26462d050e5c2f1282cb26a6cd
Roy Nielsen <rsn@rntm2.lanl.gov>
2018-02-09 16:26:11 -0700

    straightening out stuff between projects.

---
+++ proto/adminCreds.py
@@ -0,0 +1,30 @@
+import sys
+
+from PyQt5 import QtWidgets
+
+sys.path.append('..')
+
+from lib.loggers import CyLogger
+from ui.admin_creds import AdministratorCredentials
+
+logger = CyLogger(debug_mode=True)
+logger.initializeLogs()
+
+app = QtWidgets.QApplication(sys.argv)
+
+def setUserCreds(username='', password=''):
+    print str(username)
+    print str(password)
+
+#####
+# Get admin user credentials
+adminCreds = AdministratorCredentials(logger)
+adminCreds.userCreds.connect(self.setUserCreds)
+adminCreds.ui.adminName.setText('Not Today Wabbit!')
+adminCreds.ui.passwordLineEdit.setFocus()
+adminCreds.show()
+adminCreds.raise_()
+
+app.exec_()
+
+
